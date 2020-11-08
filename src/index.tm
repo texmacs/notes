6eda1e67bd2908e06ea0de40a75d9a4defe219d1
Massimiliano Gubinelli <m.gubinelli@gmail.com>
2020-11-08 19:30:35 +0100

    Small improvements to CSS styling

--- src/index.tm
+++ src/index.tm
@@ -1,6 +1,6 @@
 <TeXmacs|1.99.14>
 
-<style|notes>
+<style|<tuple|notes|html-font-size>>
 
 <\body>
   <hrule><chapter*|A TeXmacs blog>
@@ -64,15 +64,11 @@
     <associate|auto-1|<tuple|?|?>>
     <associate|auto-2|<tuple|?|?>>
     <associate|auto-3|<tuple|?|?>>
-    <associate|bib-TeXmacs:vdH:book|<tuple|1|?>>
   </collection>
 </references>
 
 <\auxiliary>
   <\collection>
-    <\associate|bib>
-      TeXmacs:vdH:book
-    </associate>
     <\associate|toc>
       <vspace*|1fn><with|font-series|<quote|bold>|math-font-series|<quote|bold>|font-shape|<quote|small-caps>|A
       TeXmacs blog> <datoms|<macro|x|<repeat|<arg|x>|<with|font-series|medium|<with|font-size|1|<space|0.2fn>.<space|0.2fn>>>>>|<htab|5mm>>
@@ -81,9 +77,8 @@
       Contents <datoms|<macro|x|<repeat|<arg|x>|<with|font-series|medium|<with|font-size|1|<space|0.2fn>.<space|0.2fn>>>>>|<htab|5mm>>
       <no-break><pageref|auto-2>
 
-      <vspace*|1fn><with|font-series|<quote|bold>|math-font-series|<quote|bold>|font-shape|<quote|small-caps>|Bibliography>
-      <datoms|<macro|x|<repeat|<arg|x>|<with|font-series|medium|<with|font-size|1|<space|0.2fn>.<space|0.2fn>>>>>|<htab|5mm>>
-      <pageref|auto-3><vspace|0.5fn>
+      Contributing <datoms|<macro|x|<repeat|<arg|x>|<with|font-series|medium|<with|font-size|1|<space|0.2fn>.<space|0.2fn>>>>>|<htab|5mm>>
+      <no-break><pageref|auto-3>
     </associate>
   </collection>
 </auxiliary>
--- src/notes.ts
+++ src/notes.ts
@@ -8,6 +8,15 @@
       <src-style-file|notes|1.0>
 
       <src-purpose|Style file for blogging.>
+
+      <src-copyright|2020|Massimiliano Gubinelli>
+
+      <\src-license>
+        This software falls under the <hlink|GNU general public license,
+        version 3 or later|$TEXMACS_PATH/LICENSE>. It comes WITHOUT ANY
+        WARRANTY WHATSOEVER. You should have received a copy of the license
+        which the software. If not, see <hlink|http://www.gnu.org/licenses/gpl-3.0.html|http://www.gnu.org/licenses/gpl-3.0.html>.
+      </src-license>
     </src-title>
   </active*>
 
@@ -25,7 +34,7 @@
 
   \;
 
-  <assign|html-title|TeXmacs notes: Preview>
+  <assign|html-title|TeXmacs notes>
 
   <assign|html-css|<tuple|./resources/notes-base.css>>
 
@@ -35,7 +44,27 @@
     </src-comment>
   </active*>
 
-  \;
+  <\active*>
+    <\src-comment>
+      Other customizations
+    </src-comment>
+  </active*>
+
+  <assign|tmhtml-render-code|<macro|body|<html-div-class|tmweb-code|<arg|body>>>>
+
+  <assign|tmhtml-pseudo-code|<macro|body|<html-div-class|tmweb-code|<arg|body>>>>
+
+  <assign|tmhtml-framed-code|<macro|body|<html-div-class|tmweb-code|<arg|body>>>>
+
+  <assign|tmhtml-framed-fragment|<macro|body|<html-div-class|tmweb-code|<arg|body>>>>
+
+  <assign|tmhtml-render-key|<macro|key|<html-class|tmweb-key|<arg|key>>>>
+
+  <assign|tmhtml-menu-item|<macro|body|<html-class|tmweb-menu|<with|font-family|ss|<localize|<arg|body>>>>>>
+
+  <assign|tmhtml-menu-extra|<macro|body|\<rightarrow\><menu-item|<arg|body>>>>
+
+  <assign|tmhtml-markup|<macro|body|<html-class|tmweb-markup|<arg|body>>>>
 </body>
 
 <initial|<\collection>
